bd73e6e7c2447743a3ad69cb252d0069436e5f82
Sara Zmak <zmaksara@gmail.com>
2025-04-02 12:32:07 +0200

    add ColorForm and ColorInput jsx templates

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
+import ColorForm from "./Components/ColorForm/ColorForm";
+
 import "./App.css";
 
 function App() {
   return (
     <>
       <h1>Theme Creator</h1>
+      <ColorForm />
 
       {initialColors.map((color) => {
         return <Color key={color.id} color={color} />;
---
+++ src/Components/ColorForm/ColorForm.jsx
@@ -0,0 +1,47 @@
+import ColorInput from "../ColorInput/ColorInput";
+import "./ColorForm.css";
+
+export default function ColorForm({
+  onSubmitColor,
+  initialData = {
+    role: "add a color",
+    hex: "#123456",
+    contrastText: "#ffffff",
+  },
+}) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    onSubmitColor(data);
+  }
+
+  return (
+    <form className="color-form" onSubmit={handleSubmit}>
+      <label htmlFor="role">
+        Role
+        <br />
+        <input
+          type="text"
+          id="role"
+          name="role"
+          defaultValue={initialData.role}
+        />
+      </label>
+      <br />
+      <label htmlFor="hex">
+        Hex
+        <br />
+        <ColorInput id="hex" defaultValue={initialData.hex} />
+      </label>
+      <br />
+      <label htmlFor="contrastText">
+        Contrast Text
+        <br />
+        <ColorInput id="contrastText" defaultValue={initialData.contrastText} />
+      </label>
+      <br />
+      <button>ADD COLOR</button>
+    </form>
+  );
+}
