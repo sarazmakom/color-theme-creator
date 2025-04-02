128823ab29f500b201635bb0ad035849a4aaefab
Sara Zmak <zmaksara@gmail.com>
2025-04-02 16:50:56 +0200

    DeleteButton - handle delete after confirm and cancel button + css

--- src/Components/DeleteButton/DeleteButton.jsx
+++ src/Components/DeleteButton/DeleteButton.jsx
@@ -1,5 +1,37 @@
 import "./DeleteButton.css";
+import { useState } from "react";
 
-export function DeleteButton() {
-  return <button className="delete-button">DELETE</button>;
+export function DeleteButton({ onDelete }) {
+  const [isConfirmed, setIsConfirmed] = useState(false);
+
+  function handleDeleteClick() {
+    if (!isConfirmed) {
+      setIsConfirmed(true);
+      return;
+    }
+    onDelete();
+    setIsConfirmed(false);
+  }
+
+  function handleCancel() {
+    setIsConfirmed(false);
+  }
+
+  return (
+    <div>
+      {isConfirmed ? (
+        <div>
+          <p className="color-card-highlight">
+            Are you sure you want to delete this color?
+          </p>
+          <button onClick={handleDeleteClick}>DELETE</button>
+          <button onClick={handleCancel}>CANCEL</button>
+        </div>
+      ) : (
+        <button className="delete-button" onClick={handleDeleteClick}>
+          DELETE
+        </button>
+      )}
+    </div>
+  );
 }
