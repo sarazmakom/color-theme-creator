c63226693035a2e71bf2dc910dd3bfc78500442e
Sara Zmak <zmaksara@gmail.com>
2025-04-02 16:52:59 +0200

    add onDelete to DeleteButton

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,7 +1,7 @@
 import "./Color.css";
 import { DeleteButton } from "../DeleteButton/DeleteButton";
-export default function Color({ color }) {
 
+export default function Color({ color, onDelete }) {
   return (
     <div
       className="color-card"
@@ -13,7 +13,7 @@ export default function Color({ color }) {
       <h3 className="color-card-headline">{color.hex}</h3>
       <h4>{color.role}</h4>
       <p>contrast: {color.contrastText}</p>
-      <DeleteButton />
+      <DeleteButton onDelete={() => onDelete(color.id)} />
     </div>
   );
 }
